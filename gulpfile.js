d184d391ca1258830684286953a1884fca20a4a6
Zhao Kang <1003994970@qq.com>
2016-03-02 22:47:56 +0800

    modify gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,45 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
+// 引入 gulp
+var gulp = require('gulp'); 
 
-gulp.task('minify', function () {
-  gulp.src('js/app.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('build'))
+// 引入组件
+var jshint = require('gulp-jshint');
+// var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// 检查脚本
+gulp.task('lint', function() {
+    gulp.src('./js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// 编译Sass
+// gulp.task('sass', function() {
+//     gulp.src('./scss/*.scss')
+//         .pipe(sass())
+//         .pipe(gulp.dest('./css'));
+// });
+
+// 合并，压缩文件
+gulp.task('scripts', function() {
+    gulp.src('./js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./build'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./build'));
+});
+
+// 默认任务
+gulp.task('default', function(){
+    // gulp.run('lint', 'sass', 'scripts');
+    gulp.run('lint', 'scripts');
+
+    // 监听文件变化
+    gulp.watch('./js/*.js', function(){
+        // gulp.run('lint', 'sass', 'scripts');
+        gulp.run('lint', 'scripts');
+    });
 });
